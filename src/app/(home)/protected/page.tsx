a270815d91af411a8611126a69e73a83c32b4eac
Matheus Silva <msilvaqs@outlook.com>
2025-04-13 19:46:58 -0400

    Videos category

--- src/app/(home)/protected/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Page = () => {
-  return <div>Only loged in users</div>;
-};
-
-export default Page;
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -18,3 +18,15 @@ export const users = pgTable(
   },
   (user) => [uniqueIndex("clerk_id_idx").on(user.clerkId)]
 );
+
+export const categories = pgTable(
+  "categories",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    name: text("name").notNull().unique(),
+    description: text("description"),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
+  },
+  (categorie) => [uniqueIndex("name_index").on(categorie.name)]
+);
