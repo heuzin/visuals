19d988f9916e87a76326cb491b5fdaec81aa46e4
Matheus Silva <msilvaqs@outlook.com>
2025-04-16 12:05:04 -0400

    Studio videos setup

--- src/app/(studio)/studio/page.tsx
+++ src/app/(studio)/studio/page.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { HydrateClient, trpc } from "@/trpc/server";
 
-const Page = () => {
-  return <div>Page</div>;
+import { DEFAULT_LIMIT } from "@/constants";
+import { StudioView } from "@/modules/studio/ui/view/studio-view";
+
+const Page = async () => {
+  void trpc.studio.getMany.prefetchInfinite({
+    limit: DEFAULT_LIMIT,
+  });
+
+  return (
+    <HydrateClient>
+      <StudioView />
+    </HydrateClient>
+  );
 };
 
 export default Page;
